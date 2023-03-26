33377033ed8be6136a65ca621edbf8766c486e46
Ujjanth Arhan <33844003+ujjanth-arhan@users.noreply.github.com>
2023-03-26 15:21:09 -0400

    House robber better memory solution

--- JavaSolutions/213 - House Robber II.java
+++ JavaSolutions/213 - House Robber II.java
@@ -2,19 +2,15 @@ class Solution {
     public int rob(int[] nums) {
         if (nums.length == 0 || nums.length == 1) return nums[0];
         if (nums.length == 2) return Math.max(nums[0], nums[1]);
-        int[] arr1 = new int[nums.length-1];
-        int[] arr2 = new int[nums.length-1];
-        System.arraycopy(nums, 0, arr1, 0, nums.length - 1);
-        System.arraycopy(nums, 1, arr2, 0, nums.length - 1);
 
-        return Math.max(maxRob(arr1), maxRob(arr2));
+        return Math.max(maxRob(0, nums.length - 1, nums), maxRob(1, nums.length, nums));
     }
 
-    private int maxRob(int[] nums) {
+    private int maxRob(int start, int end, int[] nums) {
         int fir = 0;
-        int sec = nums[0];
+        int sec = nums[start];
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = start + 1; i < end; i++) {
             int max = Math.max(fir + nums[i], sec);
             fir = sec;
             sec = max;
